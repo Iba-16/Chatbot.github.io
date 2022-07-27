9f7b1bcd93fd05ecf151f8c402bd228538c24d7e
Jasmine <jasmine@ahmads-mac-mini.wireless.concordia.ca>
2022-07-27 10:37:35 -0400

    pushing bot stuff

--- app.py
+++ app.py
@@ -10,7 +10,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(my_bot.get_response(userText))
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
